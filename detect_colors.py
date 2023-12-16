882040e8c1e7453e5084116b430ae2d0aaa4b1b2
f1rA5h <shtey.alexey@gmail.com>
2023-12-16 19:47:04 +0300

    color definition updated

--- detect_colors.py
+++ detect_colors.py
@@ -2,18 +2,22 @@ import cv2
 import numpy as np
 
 def detect_colors(bgr: tuple) -> str:
+    hsv_color = cv2.cvtColor(np.uint8([[bgr]]), cv2.COLOR_BGR2HSV)[0][0]
     b, g, r = map(int, bgr)
-    if b <= 35 and g <= 35 and r >= 60:
-        return 'red'
-    elif b >= 90 and g <= 90 and r <= 60:
-        return 'blue'
-    elif b <= 30 and g >= 30 and r <= 20:
-        return 'green'
-    elif b <= 60 and g >= 100 and r >= 100:
-        return 'yellow'
-    elif b <= 20 and g <= 20 and r <= 20:
-        return 'black'
+    
+    # Определяем цвет по оттенку
+    hue = hsv_color[0]
+
+    if b <= 20 and g <= 20 and r <= 20:
+        return "black"
+    elif hue <= 10 or hue > 160:
+        return "red"
+    elif 10 < hue <= 45:
+        return "yellow"
+    elif 100 < hue <= 130:
         return "blue"
+    elif 45 < hue <= 70:
+        return "green"
     else:
         return "unknown"
 
